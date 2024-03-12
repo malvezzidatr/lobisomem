de4434c395625f06ca50ec1803567a470418c9f5
Caio Malvezzi <malvezzi.dev@gmail.com>
2024-03-12 16:44:49 -0300

    UI da home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,16 +1,16 @@
 import React from "react"
 import * as S from './styles';
 import { LinearGradient } from "expo-linear-gradient";
-import { Image, View } from "react-native";
+import { Image, Text, TouchableOpacity, View } from "react-native";
 
 export const Home = () => {
     return (
         <LinearGradient
             colors={['#3A3A50', '#87CEEB']}
-            style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}
+            style={{flex: 11, alignItems: 'center', justifyContent: 'space-around', paddingHorizontal: 32}}
         >
-            <View style={{position: 'relative', flexDirection: 'row', alignItems: 'center'}}>
-            <Image
+            <View style={{position: 'relative', flexDirection: 'row', alignItems: 'center', flex: 5}}>
+                <Image
                     source={require('../../assets/moon.png')}
                     style={{position: 'absolute', right: -8, zIndex: 1}}
                 />
@@ -25,6 +25,23 @@ export const Home = () => {
                     MEM
                 </S.Title>
             </View>
+
+            <View style={{width: '100%', flex: 6}}>
+                <TouchableOpacity style={{flexDirection: 'row', alignItems: 'center', justifyContent: 'center', backgroundColor: '#3A3A50', width: '100%', height: 60, borderRadius: 10}}>
+                    <Image
+                        source={require('../../assets/handIcon.png')}
+                        style={{marginRight: 12}}
+                    />
+                    <Text style={{fontSize: 24, color: 'white'}}>Novo Jogo</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={{flexDirection: 'row', alignItems: 'center', justifyContent: 'center', backgroundColor: '#FFF', width: '100%', height: 60, borderRadius: 10, marginTop: 22}}>
+                    <Text style={{fontSize: 20, color: '#3A3A50'}}>Conectar a um lobby</Text>
+                </TouchableOpacity>
+            </View>
+            <Image
+                source={require('../../assets/sun.png')}
+                style={{position: 'absolute', bottom: 0}}
+            />
         </LinearGradient>
     )
 }
