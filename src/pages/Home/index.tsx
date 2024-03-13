27ee81459544a568d68b4e28ed950f5a0bdf8cd1
Caio Malvezzi <malvezzi.dev@gmail.com>
2024-03-13 09:18:11 -0300

    Ajuste de estilos

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,47 +1,44 @@
 import React from "react"
 import * as S from './styles';
-import { LinearGradient } from "expo-linear-gradient";
-import { Image, Text, TouchableOpacity, View } from "react-native";
+import { Image, Text } from "react-native";
 
 export const Home = () => {
     return (
-        <LinearGradient
+        <S.Container
             colors={['#3A3A50', '#87CEEB']}
-            style={{flex: 11, alignItems: 'center', justifyContent: 'space-around', paddingHorizontal: 32}}
         >
-            <View style={{position: 'relative', flexDirection: 'row', alignItems: 'center', flex: 5}}>
-                <Image
+            <S.ContainerTitle>
+                <S.Moon
                     source={require('../../assets/moon.png')}
-                    style={{position: 'absolute', right: -8, zIndex: 1}}
                 />
                 <S.Title>
                     LOBIS
                 </S.Title>
-                <Image
+                <S.WolfTitle
                     source={require('../../assets/wolfTitle.png')}
                     style={{height: 50}}
                 />
                 <S.Title>
                     MEM
                 </S.Title>
-            </View>
+            </S.ContainerTitle>
 
-            <View style={{width: '100%', flex: 6}}>
-                <TouchableOpacity style={{flexDirection: 'row', alignItems: 'center', justifyContent: 'center', backgroundColor: '#3A3A50', width: '100%', height: 60, borderRadius: 10}}>
+            <S.MenuContainer>
+                <S.NemGameButton>
                     <Image
                         source={require('../../assets/handIcon.png')}
                         style={{marginRight: 12}}
                     />
                     <Text style={{fontSize: 24, color: 'white'}}>Novo Jogo</Text>
-                </TouchableOpacity>
-                <TouchableOpacity style={{flexDirection: 'row', alignItems: 'center', justifyContent: 'center', backgroundColor: '#FFF', width: '100%', height: 60, borderRadius: 10, marginTop: 22}}>
+                </S.NemGameButton>
+                <S.LobbyButton>
                     <Text style={{fontSize: 20, color: '#3A3A50'}}>Conectar a um lobby</Text>
-                </TouchableOpacity>
-            </View>
+                </S.LobbyButton>
+            </S.MenuContainer>
             <Image
                 source={require('../../assets/sun.png')}
                 style={{position: 'absolute', bottom: 0}}
             />
-        </LinearGradient>
+        </S.Container>
     )
 }
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,6 +1,57 @@
+import { LinearGradient } from "expo-linear-gradient";
 import styled from "styled-components/native";
+
+export const Container = styled(LinearGradient)`
+    flex: 11;
+    align-items: 'center';
+    justify-content: 'space-around';
+    padding: 0 32px;
+`
+
+export const ContainerTitle = styled.View`
+    position: 'relative';
+    flex-direction: 'row';
+    align-items: 'center';
+    flex: 5;
+`
+
+export const Moon = styled.Image`
+    position: 'absolute';
+    right: -8;
+    z-index: 1;
+`
+
+export const WolfTitle = styled.Image`
+    height: 50px;
+`
 
 export const Title = styled.Text`
     font-size: 56px;
     color: white;
+`
+
+export const MenuContainer = styled.View`
+    width: '100%';
+    flex: 6;
+`
+
+export const NemGameButton = styled.TouchableOpacity`
+    flex-direction: 'row';
+    align-items: 'center';
+    justify-content: 'center';
+    background-color: '#3A3A50';
+    width: '100%';
+    height: 60px;
+    border-radius: 10px
+`
+
+export const LobbyButton = styled.TouchableOpacity`
+    flex-direction: 'row';
+    align-items: 'center';
+    justify-content: 'center';
+    background-color: '#FFF';
+    width: '100%';
+    height: 60px;
+    border-radius: 10px;
+    margin-top: 22px;
 `
